5b275de0104eb3986ad95018b25981ca166be26f
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-06-04 20:08:46 -0300

    ng g c template-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './template/header/header.component';
 import { FooterComponent } from './template/footer/footer.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TemplateFormComponent
   ],
   imports: [
     BrowserModule,
