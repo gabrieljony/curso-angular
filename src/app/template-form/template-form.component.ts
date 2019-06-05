0c82ec1d9ce32d20ce7b9d05bb70a8c705d96351
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-06-05 08:08:15 -0300

    Submetendo valores com HTTP POST

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -26,10 +26,15 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSubmit(form) {
-    console.log(form.value);
-    console.log(form);
+  onSubmit(formulario) {
+    console.log(formulario.value);
+    console.log(formulario);
     console.log(this.usuario);
+    this.http.post('https://httpbin.org/post', JSON.stringify(formulario.value))
+    .subscribe(dados => {
+      console.log(dados);
+      formulario.form.reset();
+    });
   }
 
   verificaValidTouched(campo) {
@@ -44,10 +49,13 @@ export class TemplateFormComponent implements OnInit {
 
   consultaCEP(cep) {
     console.log(cep);
+
     //Nova variável "cep" somente com dígitos.
     cep = cep.replace(/\D/g, '');
+
     //Verifica se campo cep possui valor informado.
     if (cep != "") {
+
       //Expressão regular para validar o CEP.
       var validacep = /^[0-9]{8}$/;
       //Valida o formato do CEP.
