4194413c91c4e1d67378517d2bdeb5e7e3fb601c
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-06-04 21:55:40 -0300

    Criado componet form-debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DataFormComponent } from './data-form/data-form.component';
+import { FormDebugComponent } from './form-debug/form-debug.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent
+    DataFormComponent,
+    FormDebugComponent
   ],
   imports: [
     BrowserModule,
