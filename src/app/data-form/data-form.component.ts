f578dd375b84249ef2406410939047e791c9003e
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-06-06 06:53:49 -0300

    Verificar validação dos campos com FormGroup

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -23,36 +23,36 @@ export class DataFormComponent implements OnInit {
   // FormGroup
   //Instanciar uma classe FormGroup e a classe recebe um objeto como parâmetro - nome e email
 
-  // this.formulario = new FormGroup({
-  //   nome: new FormControl(null),
-  //   email: new FormControl(null),
-  //   endereco: new FormGroup({
-  //     cep: new FormControl(null),
-  //     numero: new FormControl(null),
-  //     complemento: new FormControl(null),
-  //     rua: new FormControl(null),
-  //     bairro: new FormControl(null),
-  //     cidade: new FormControl(null),
-  //     estado: new FormControl(null)
-  //   })
-  // });
+  this.formulario = new FormGroup({
+    nome: new FormControl(null, Validators.required),
+    email: new FormControl(null,[ Validators.required, Validators.email]),
+    endereco: new FormGroup({
+      cep: new FormControl(null, Validators.required),
+      numero: new FormControl(null, Validators.required),
+      complemento: new FormControl(null),
+      rua: new FormControl(null, Validators.required),
+      bairro: new FormControl(null, Validators.required),
+      cidade: new FormControl(null, Validators.required),
+      estado: new FormControl(null, Validators.required)
+    })
+  });
 
   // FormBuilder
   //Instanciar uma classe usando o FormBuilder e a classe recebe um objeto como parâmetro
 
-  this.formulario = this.formBuilder.group({
-    nome:[null, Validators.required],
-    email:[null, [Validators.required, Validators.email]],
-    endereco: this.formBuilder.group({
-      cep:[null, Validators.required],
-      numero:[null, Validators.required],
-      complemento:[null],
-      rua:[null, Validators.required],
-      bairro:[null, Validators.required],
-      cidade:[null, Validators.required],
-      estado:[null, Validators.required]
-    })
-  });
+  // this.formulario = this.formBuilder.group({
+  //   nome:[null, Validators.required],
+  //   email:[null, [Validators.required, Validators.email]],
+  //   endereco: this.formBuilder.group({
+  //     cep:[null, Validators.required],
+  //     numero:[null, Validators.required],
+  //     complemento:[null],
+  //     rua:[null, Validators.required],
+  //     bairro:[null, Validators.required],
+  //     cidade:[null, Validators.required],
+  //     estado:[null, Validators.required]
+  //   })
+  // });
 
 
   }
