01314cfa97accf9256d3b346a26825b430ea97f8
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-06-05 21:14:17 -0300

    instanciar usando FormGroup ou FormBuilder

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -10,9 +10,22 @@ export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
+  //No momento da inicialização do component chama o ngOnInit
   ngOnInit() {
+  //Instanciar uma classe FormGroup e a classe recebe um objeto como parâmetro - nome e email
+  this.formulario = new FormGroup({
+    nome: new FormControl(null),
+    email: new FormControl(null)
+  });
+  //Instanciar uma classe usando o FormBuilder e a classe recebe um objeto como parâmetro
+  this.formulario = this.formBuilder.group({
+    nome:[null],
+    email:[null]
+  });
+
+
   }
 
 }
