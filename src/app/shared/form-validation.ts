7e78f4befc3bb1e5822b5d4eb861665c5ab7eabf
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-06-21 18:15:09 -0300

    Validação entre dois campos (confirmar email)

--- src/app/shared/form-validation.ts
+++ src/app/shared/form-validation.ts
@@ -2,28 +2,37 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 export class FormValidations {
 
+  //metodo generico para comparar 2 inputs - foi usado no confirmar e-mail
   static equalsTo(otherField: string) {
     const validator = (formControl: FormControl) => {
       if (otherField == null) {
         throw new Error ('É necessário informar um campo.');
       }
 
+      //console.log((<FormGroup>formControl.root));
+      //console.log((<FormGroup>formControl.root).get(otherField));
+
+      //formulario tem que ser renderizado
       if (!formControl.root || !(<FormGroup>formControl.root).controls) {
         return null;
       }
 
+      //console.log((<FormGroup>formControl.root));
+      //console.log((<FormGroup>formControl.root).get(otherField));
+
       const field = (<FormGroup>formControl.root).get(otherField);
 
+      //se o campo não existir
       if (!field) {
-        throw new Error('É necessário informar um campo válido.');
+        throw new Error('É necessário informar um campo válido, este campo não existe.');
       }
 
+      //faz a comparação
       if (field.value !== formControl.value) {
-        return {
-          equalsTo: otherField
-        };
+        return { equalsTo: otherField };//pode retornar otherField ou true
       }
 
+      //se o campo for igual vai retornar nulo para dizer que o campo está válido
       return null;
 
     };
