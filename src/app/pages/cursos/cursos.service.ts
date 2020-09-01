936be13d349b125025ce6592e6a22e2bee2465b6
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2020-09-01 11:33:17 -0300

    Http: Dica: Variável de Ambiente

--- src/app/pages/cursos/cursos.service.ts
+++ src/app/pages/cursos/cursos.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CursoModel } from './curso.model';
+import { environment } from './../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CursosService {
   // readonly não posso atualizar essa variavel
-  private readonly API = 'http://localhost:3000/cursos';
+  private readonly API = `${environment.API}cursos`;
 
   constructor(private http: HttpClient) {}
 
