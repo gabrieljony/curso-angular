fb74bdf93eee479d9e0b47c13466283efb3d13bb
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2020-09-01 08:23:58 -0300

    Http / HttpClient: Introdução

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { DataFormComponent } from './pages/data-form/data-form.component';
 import { TemplateFormComponent } from './pages/template-form/template-form.component';
 import { DataFormBuilderComponent } from './pages/data-form-builder/data-form-builder.component';
 import { DataFormFullComponent } from './pages/data-form-full/data-form-full.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'template-form', component: TemplateFormComponent },
   { path: 'data-form', component: DataFormComponent },
   { path: 'data-form-full', component: DataFormFullComponent },
   { path: 'data-form-builder', component: DataFormBuilderComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'data-form-builder' },
+  { path: 'cursos', loadChildren: './pages/cursos/cursos.module#CursosModule' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,3 @@
-import { ListService } from './services/list.service';
-import { ConsultarCepService } from './services/consultar-cep.service';
-import { VerificarEmailService } from './services/verificar-email.service';
-import { DataFormFullModule } from './pages/data-form-full/data-form-full.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -12,6 +8,12 @@ import { TemplateModule } from './template/template.module';
 import { TemplateFormModule } from './pages/template-form/template-form.module';
 import { DataFormModule } from './pages/data-form/data-form.module';
 import { DataFormBuilderModule } from './pages/data-form-builder/data-form-builder.module';
+import { HomeModule } from './pages/home/home.module';
+import { CursosModule } from './pages/cursos/cursos.module';
+import { ListService } from './services/list.service';
+import { ConsultarCepService } from './services/consultar-cep.service';
+import { VerificarEmailService } from './services/verificar-email.service';
+import { DataFormFullModule } from './pages/data-form-full/data-form-full.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,6 +27,8 @@ import { DataFormBuilderModule } from './pages/data-form-builder/data-form-build
     DataFormModule,
     DataFormFullModule,
     DataFormBuilderModule,
+    CursosModule,
+    HomeModule
   ],
   providers: [VerificarEmailService, ConsultarCepService, ListService],
   bootstrap: [AppComponent],
