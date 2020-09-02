ce856268021a2d11eb3633fe7b70f9c2dc885852
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2020-09-02 10:36:20 -0300

    HTTP POST Criando Cursos - Testando

--- src/app/pages/cursos/cursos-form/cursos-form.component.ts
+++ src/app/pages/cursos/cursos-form/cursos-form.component.ts
@@ -20,7 +20,7 @@ export class CursosFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      nome: [
+      name: [
         ,
         [
           Validators.required,
